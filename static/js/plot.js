bd8e0bb9735d222ccadcbcdab562b68600b513de
kayburns <kaylee.c.burns@gmail.com>
2023-09-19 17:23:03 -0700

    Update plot.js

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,5 +1,8 @@
 TESTER = document.getElementById('tester');
-Plotly.newPlot( TESTER, [{
-x: [0, 0, 27.3, 45.9, 37.5, 26.5, 0, 0, 41.9, 0, 38.6, 23.7, 34.4, 27.2, 44.1],
-y: [4.4, 6.6, 7.3, 8.9, 9.0, 10.7, 11.2, 11.7, 13.9, 14.4, 15.1, 15.1, 18.0, 18.8] }], {
+Plotly.newPlot( TESTER, [
+  {
+    x: [0, 0, 27.3, 45.9, 37.5, 26.5, 0, 0, 41.9, 0, 38.6, 23.7, 34.4, 27.2, 44.1],
+    y: [4.4, 6.6, 7.3, 8.9, 9.0, 10.7, 11.2, 11.7, 13.9, 14.4, 15.1, 15.1, 18.0, 18.8],
+    text: ['RN-INSUP', 'RN-DINO', 'ViT-INSUP', 'ViT-DINO', 'SIN-SUP', 'VIP', 'MVP', 'R3M', 'MoCo. v3, ViT', 'MoCo. v3, RN', 'Dino. v2, ViT', 'MVP, ViT-S (HOI)', 'CLIP, ViT-B/16', 'MAE-IN, ViT-S', 'MoCo. v3, ViT-B'],
+  }], {
 margin: { t: 0 } } );
